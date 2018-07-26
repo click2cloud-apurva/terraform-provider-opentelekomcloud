9663633e1cea4189d31bdf377c6bee17e5073b56
rohini.burde <rohini.burde@click2cloud.net>
2018-07-26 15:44:44 +0530

    Execute multiple test cases

--- vpc_route.tf
+++ vpc_route.tf
@@ -1,26 +1,31 @@
 
+##TestCase1:  Positive scenario to verify resources are created successfully.
 
-resource "opentelekomcloud_vpc_v1" "vpc_1_positivetestcase" {
-  name = "vpc_test-roh"
+resource "opentelekomcloud_vpc_v1" "vpc_1_testcase1" {
+  name = "vpc_1_testcase1"
   cidr = "192.168.0.0/16"
 }
-resource "opentelekomcloud_vpc_v1" "vpc_2_positivetestcase" {
-  name = "vpc_test1-roh"
+resource "opentelekomcloud_vpc_v1" "vpc_2_testcase1" {
+  name = "vpc_2_testcase1"
   cidr = "192.168.0.0/16"
 }
-resource "opentelekomcloud_vpc_peering_connection_v2" "peering_1_positivetestcase" {
-  name = "opentelekomcloud_peering"
-  vpc_id = "${opentelekomcloud_vpc_v1.vpc_1_positivetestcase.id}"
-  peer_vpc_id = "${opentelekomcloud_vpc_v1.vpc_2_positivetestcase.id}"
+resource "opentelekomcloud_vpc_peering_connection_v2" "peering_1_testcase1" {
+  name = "peering_1_testcase1"
+  vpc_id = "${opentelekomcloud_vpc_v1.vpc_1_testcase1.id}"
+  peer_vpc_id = "${opentelekomcloud_vpc_v1.vpc_2_testcase1.id}"
 }
 resource "opentelekomcloud_vpc_route_v2" "route_1_positivetestcase" {
   type = "peering"
-  nexthop = "${opentelekomcloud_vpc_peering_connection_v2.peering_1_positivetestcase.id}"
+  nexthop = "${opentelekomcloud_vpc_peering_connection_v2.peering_1_testcase1.id}"
   destination = "192.168.0.0/16"
-  vpc_id = "${opentelekomcloud_vpc_v1.vpc_1_positivetestcase.id}"
+  vpc_id = "${opentelekomcloud_vpc_v1.vpc_1_testcase1.id}"
 
 }
 
+
+##TestCase2:  Negative scenario to verify resource getting created with invalid name parameter in opentelekomcloud_vpc_v1 resource.
+
+/*
 resource "opentelekomcloud_vpc_v1" "vpc_1_negativetestcase" {
   name = "vpc_test-roh2"
   cidr = "192.168.0.0/17"
@@ -41,3 +46,4 @@ resource "opentelekomcloud_vpc_route_v2" "route_1_negativetestcase" {
   vpc_id = "${opentelekomcloud_vpc_v1.vpc_2_negativetestcase.id}"
 
 }
+*/
