709e37b85ea201a2cfb70f29646deb9c8e6a5550
rohini.burde <rohini.burde@click2cloud.net>
2018-08-01 15:33:56 +0530

    Test multiple negative testcases with one +ve test case in negative scenario - error solved

--- Negative_TestCases/WithAllMandetoryParameters.tf
+++ Negative_TestCases/WithAllMandetoryParameters.tf
@@ -1,23 +1,23 @@
 
 ##TestCase1:  To veify resources are created with all the mandetory parameters.
 
-resource "opentelekomcloud_vpc_v1" "vpc_1_testcase1" {
-  name = "vpc_1_testcase1"
+resource "opentelekomcloud_vpc_v1" "vpc_1_testcase5" {
+  name = "vpc_1_testcase5"
   cidr = "192.168.0.0/16"
 }
-resource "opentelekomcloud_vpc_v1" "vpc_2_testcase1" {
-  name = "vpc_2_testcase1"
+resource "opentelekomcloud_vpc_v1" "vpc_2_testcase5" {
+  name = "vpc_2_testcase5"
   cidr = "192.168.0.0/16"
 }
-resource "opentelekomcloud_vpc_peering_connection_v2" "peering_1_testcase1" {
-  name = "peering_1_testcase1"
-  vpc_id = "${opentelekomcloud_vpc_v1.vpc_1_testcase1.id}"
-  peer_vpc_id = "${opentelekomcloud_vpc_v1.vpc_2_testcase1.id}"
+resource "opentelekomcloud_vpc_peering_connection_v2" "peering_1_testcase5" {
+  name = "peering_1_testcase5"
+  vpc_id = "${opentelekomcloud_vpc_v1.vpc_1_testcase5.id}"
+  peer_vpc_id = "${opentelekomcloud_vpc_v1.vpc_2_testcase5.id}"
 }
-resource "opentelekomcloud_vpc_route_v2" "route_1_testcase1" {
+resource "opentelekomcloud_vpc_route_v2" "route_1_testcase5" {
   type = "peering"
-  nexthop = "${opentelekomcloud_vpc_peering_connection_v2.peering_1_testcase1.id}"
+  nexthop = "${opentelekomcloud_vpc_peering_connection_v2.peering_1_testcase5.id}"
   destination = "192.168.0.0/16"
-  vpc_id = "${opentelekomcloud_vpc_v1.vpc_1_testcase1.id}"
+  vpc_id = "${opentelekomcloud_vpc_v1.vpc_1_testcase5.id}"
 
 }
